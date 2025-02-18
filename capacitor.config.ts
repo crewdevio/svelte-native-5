b497be6dc2c61ab2d187a188f528d1157e3d9fd6
buttercubz <ericksosagarcias@gmail.com>
2025-02-18 00:43:10 -0400

    chore: update to capacitor 7

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,6 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.svelte.app",
   appName: "svelte-native",
-  bundledWebRuntime: true,
   webDir: "dist",
   plugins: {
     SplashScreen: {
